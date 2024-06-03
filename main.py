515bdfe404fb6605e3bc75b61faef2a75795cd0d
shivnagar232 <154045191+shivnagar232@users.noreply.github.com>
2024-06-03 20:39:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import time
 import logging
+import asyncio
 from pyrogram import Client, filters, enums
 from config import session_string, allowed_groups, owner_id, extract_links
-from downloader import check_url_patterns_async, fetch_download_link_async, get_formatted_size_async, my_headers
+from downloader import check_url_patterns_async, fetch_download_link_async, get_formatted_size_async
 
 API_ID = "19748984"  # Replace with your API_ID from config.py
 API_HASH = "2141e30f96dfbd8c46fbb5ff4b197004"  # Replace with your API_HASH from config.py
 BOT_TOKEN = "6399306217:AAFpGkkyZSOZoQJuMg5bR6AV4P-OmbFs1a4"  # Replace with your BOT_TOKEN from config.py
 
 app = Client(
-    name="trbx",
+    session_string,
     bot_token=BOT_TOKEN,
     api_id=int(API_ID),
     api_hash=API_HASH
@@ -80,15 +81,26 @@ async def link_handler(client, message):
                 time_taken = end_time - start_time
                 await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
 
-                link_message = "\n\n".join([await format_message(link) for link in link_data])
-                download_message = (
-                    f"🔗 <b>Link Bypassed!</b>\n\n{link_message}\n\n<b>Time Taken</b>: {time_taken:.2f} seconds"
-                )
+                if link_data:
+                    for link in link_data:
+                        file_name = link["server_filename"]
+                        download_link = link["dlink"]
+                        file_size = await get_formatted_size_async(link["size"])
+
+                        # Download the file
+                        async with client.download(download_link, file_name=file_name) as file_path:
+                            # Upload the downloaded file
+                            await client.send_document(message.chat.id, file_path, caption=f"🔗 <b>Link Bypassed!</b>\n\n"
+                                                                                              f"┎ <b>Title</b>: `{file_name}`\n"
+                                                                                              f"┠ <b>Size</b>: `{file_size}`\n"
+                                                                                              f"┖ <b>Link</b>: <a href={download_link}>Link</a>")
+
+                download_message = f"🔗 <b>Link Bypassed!</b>\n\n<b>Time Taken</b>: {time_taken:.2f} seconds"
                 await process_url.edit_text(download_message)
 
         except Exception as e:
             await message.reply_text(f"Error: {e}", quote=True)
 
 
-# run the application
+# Run the application
 app.run()
