3fa7e6ef31c2376b1255abac2a604c8e961e5370
shivnagar232 <154045191+shivnagar232@users.noreply.github.com>
2024-06-03 21:37:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,11 @@ app = Client(
 
 logging.basicConfig(level=logging.INFO)
 
-# Function to download using aria2c and get downloaded file path
 async def download_with_aria2c(download_link):
     try:
         current_time = int(time.time())
         output_filename = f"@devggn_{current_time}"
-        download_process = subprocess.Popen(['aria2c', '-x', '16', '-s', '16', '-o', output_filename, download_link], stdout=subprocess.PIPE)
+        download_process = subprocess.Popen([aria2c, '-x', '16', '-s', '16', '-o', output_filename, download_link], stdout=subprocess.PIPE)
         stdout, stderr = download_process.communicate()
         if download_process.returncode == 0:
             # Assuming file is downloaded to current directory
@@ -41,19 +40,47 @@ async def download_with_aria2c(download_link):
 async def send_document_to_user(chat_id, file_path, caption):
     try:
         await app.send_document(chat_id, file_path, caption=caption)
-        os.remove(file_path)  # Remove the file after uploading
         return True
     except Exception as e:
-        logging.error(f"Error sending document or deleting file: {e}")
+        logging.error(f"Error in sending document to user: {e}")
         return False
 
-# Event handler for handling commands and message regex
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    if message.chat.type.value != "private" and str(message.chat.id) not in allowed_groups:
+        await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
+        await message.reply_text("⚠️ Forbidden!\nFor groups access.\nContact @devggn", quote=True)
+        return
+    else:
+        await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
+        await message.reply_text(
+            "Hello! I'm Terabox link Bypass Bot. Send me a link to bypass.\n"
+            "Owner: @devggn\n"
+            "Eg:- `https://teraboxapp.com/s/1Ykohv-bhT4SJJEgyDMeS-A`", quote=True)
+
+@app.on_message(filters.command("ping"))
+async def ping(client, message):
+    if str(message.from_user.id) != owner_id:
+        return
+    await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
+    start_time = time.time()
+    sent_message = await message.reply_text("Pong!", quote=True)
+    end_time = time.time()
+    time_taken = end_time - start_time
+    await sent_message.edit_text(f"Pong!\nTime Taken: {time_taken:.2f} seconds")
+
+async def format_message(link_data):
+    file_name = link_data["server_filename"]
+    file_size = await get_formatted_size_async(link_data["size"])
+    download_link = link_data["dlink"]
+    return f"┎ <b>Title</b>: `{file_name}`\n┠ <b>Size</b>: `{file_size}`\n┖ <b>Link</b>: <a href={download_link}>Link</a>"
+
 @app.on_message(filters.regex(
     pattern=r"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"))
 async def link_handler(client, message):
     if message.chat.type.value != "private" and str(message.chat.id) not in allowed_groups:
         await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-        await message.reply_text("⚠️ Forbidden! For groups access.\nContact @DTMK_C", quote=True)
+        await message.reply_text("⚠️ Forbidden! For groups access.\nContact @devggn", quote=True)
         return
     else:
         start_time = time.time()
@@ -71,49 +98,30 @@ async def link_handler(client, message):
 
                 await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
                 process_url = await message.reply_text("🔎 Processing URL...", quote=True)
-                download_link = await fetch_download_link_async(url)
+                link_data = await fetch_download_link_async(url)
 
                 end_time = time.time()
                 time_taken = end_time - start_time
                 await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
 
-                file_path = await download_with_aria2c(download_link)
+                link_message = "\n\n".join([await format_message(link) for link in link_data])
+                download_message = (
+                    f"🔗 <b>Link Bypassed!</b>\n\n{link_message}\n\n<b>Time Taken</b>: {time_taken:.2f} seconds"
+                )
+                await process_url.edit_text(download_message)
 
-                if file_path:
-                    link_message = f"🔗 <b>Link Bypassed!</b>\n\n<b>Time Taken</b>: {time_taken:.2f} seconds"
-                    await process_url.edit_text(link_message)
-                    await send_document_to_user(message.chat.id, file_path, caption="Downloaded File")
-                else:
-                    await message.reply_text("⚠️ Error downloading file.", quote=True)
+                # Example using aria2c for download
+                if link_data:
+                    download_link = link_data[0]["dlink"]  # Assuming first link in list
+                    downloaded_file = await download_with_aria2c(download_link)
+                    if downloaded_file:
+                        caption = "Downloaded file"
+                        await send_document_to_user(message.chat.id, downloaded_file, caption)
+                    else:
+                        await client.send_message(message.chat.id, "Failed to download.")
 
         except Exception as e:
             await message.reply_text(f"Error: {e}", quote=True)
 
-# Event handler for start command
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    if message.chat.type.value != "private" and str(message.chat.id) not in allowed_groups:
-        await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-        await message.reply_text("⚠️ Forbidden!\nFor groups access.\nContact @DTMK_C", quote=True)
-        return
-    else:
-        await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-        await message.reply_text(
-            "Hello! I'm Terabox link Bypass Bot. Send me a link to bypass.\n"
-            "Owner: @DTMK_C\n"
-            "Eg:- `https://teraboxapp.com/s/1Ykohv-bhT4SJJEgyDMeS-A`", quote=True)
-
-# Event handler for ping command
-@app.on_message(filters.command("ping"))
-async def ping(client, message):
-    if str(message.from_user.id) != owner_id:
-        return
-    await client.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-    start_time = time.time()
-    sent_message = await message.reply_text("Pong!", quote=True)
-    end_time = time.time()
-    time_taken = end_time - start_time
-    await sent_message.edit_text(f"Pong!\nTime Taken: {time_taken:.2f} seconds")
-
-# Run the application
+# run the application
 app.run()
